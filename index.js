3299b81198807ada1904d0ae85a927eff40c5a0f
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-07-05 22:36:44 +0300

    3.8 puhelinluettelon backend

--- index.js
+++ index.js
@@ -29,7 +29,10 @@ let persons = [
           }
         ]
     
-app.use(morgan('tiny'))
+
+morgan.token('message', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :message'))
+//app.use(morgan('tiny'))
 
 //getall
 app.get('/api/persons', (req, res) => {
