db1f2a51b05b041e79c6f96bd047778361a86332
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-07-09 10:37:52 +0300

    uibuild

---
+++ build/precache-manifest.3871be464d30ecfdaf29e6a1ba2437d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9536eca651673456cbf6e9d871a33e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b462052b25a0f70495e",
+    "url": "/static/css/main.920800fd.chunk.css"
+  },
+  {
+    "revision": "3cecff66644f73fc80c9",
+    "url": "/static/js/2.357f7ab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.357f7ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b462052b25a0f70495e",
+    "url": "/static/js/main.c5750b5f.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
